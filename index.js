d8c9563ad6d8d461d977156d2e78a523446e183b
Andrew Stacy <astacy@stratacache.com>
2017-01-02 14:13:38 -0500

    Debugging statement.

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var SERVER_CONFIG = require('./config.json');
  */
 function main() {
 
-  console.log("Made it to main.", POST.build_status);
+  console.log("Made it to main.", POST);
 
   if (POST.build_status == "success") {
 
